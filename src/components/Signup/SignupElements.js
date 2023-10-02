eeb6598217fc5e826eddb0a3f0fa419cd87a46a9
kayzen-x <mikeonochie@outlook.com>
2023-10-02 23:43:27 +0100

    added formwrap

--- src/components/Signup/SignupElements.js
+++ src/components/Signup/SignupElements.js
@@ -13,6 +13,18 @@ export const Container = styled.div`
     background: radial-gradient(circle, rgba(243,149,64,1) 20%, rgba(61,13,1,1) 90%, rgba(0,0,0,1) 100%);
 `
 
+
+export const FormWrap = styled.div`
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+
+    @media screen and (max-width: 400px) {
+        height: 80%;
+    }
+`
+
 export const FormContent = styled.div`
     height: 100%;
     display: flex;
