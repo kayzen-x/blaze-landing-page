c9cc56cb4ac064591b124876267af0924b631374
kayzen-x <mikeonochie@outlook.com>
2023-10-02 23:36:32 +0100

    added signup component

---
+++ src/components/Signup/SignupElements.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    min-height: 692px;
+    position: fixed;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 0;
+    overflow: hidden;
+    background: radial-gradient(circle, rgba(243,149,64,1) 20%, rgba(61,13,1,1) 90%, rgba(0,0,0,1) 100%);
