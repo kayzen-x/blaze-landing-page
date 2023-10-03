3cb9179a027bffba26593cedd0264dcffb7dc93e
michael-ono <mikeonochie@outlook.com>
2023-10-03 10:43:07 +0100

    add more signup components

--- src/components/Signup/SignupElements.js
+++ src/components/Signup/SignupElements.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 
@@ -11,7 +12,7 @@ export const Container = styled.div`
     z-index: 0;
     overflow: hidden;
     background: radial-gradient(circle, rgba(243,149,64,1) 20%, rgba(61,13,1,1) 90%, rgba(0,0,0,1) 100%);
-`
+`;
 
 
 export const FormWrap = styled.div`
@@ -23,7 +24,23 @@ export const FormWrap = styled.div`
     @media screen and (max-width: 400px) {
         height: 80%;
     }
-`
+`;
+
+
+export const Icon = styled(Link)`
+    margin-left: 32px;
+    margin-top: 32px;
+    text-decoration: none;
+    color: #fff;
+    font-weight: 700;
+    font-size: 32px;
+
+    @media screen and (max-width: 480px) {
+        margin-left: 16px;
+        margin-top: 8px;
+    }
+`;
+
 
 export const FormContent = styled.div`
     height: 100%;
@@ -34,7 +51,7 @@ export const FormContent = styled.div`
     @media screen and (max-width: 480px) {
         padding: 10px;
     }
-`
+`;
 
 export const Form = styled.form`
     background: #010101;
@@ -51,4 +68,42 @@ export const Form = styled.form`
     @media screen and (max-width: 400px) {
         padding: 32px 32px;
     }
-`
+`;
+
+export const FormH1 = styled.h1`
+    margin-bottom: 40px;
+    color: #fff;
+    font-size: 20px;
+    font-weight: 400;
+    text-align: center;
+`;
+
+export const FormLabel = styled.label`
+    margin-bottom: 8px;
+    font-size: 14px;
+    color: #fff;
+`;
+
+export const FormInput = styled.input`
+    padding: 16px 16px;
+    margin-bottom: 32px;
+    border: none;
+    border-radius: 4px;
+`;
+
+export const FormButton = styled.button`
+    background: #F39540;
+    padding: 16px 0;
+    border: none;
+    border-radius: 4px;
+    color: #fff;
+    font-size: 20px;
+    cursor: pointer;
+`;
+
+export const Text = styled.span`
+    text-align: center;
+    margin-top: 24px;
+    color: #fff;
+    font-size: 14px;
+`;    
