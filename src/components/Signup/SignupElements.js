90252d94030793f1bab2d316b51f8871b1600554
michael-ono <mikeonochie@outlook.com>
2023-10-19 20:52:54 +0100

    new text var

--- src/components/Signup/SignupElements.js
+++ src/components/Signup/SignupElements.js
@@ -1,5 +1,5 @@
+import { Link } from "react-router-dom";
 import styled from "styled-components";
-
 
 export const Container = styled.div`
     min-height: 692px;
@@ -11,8 +11,7 @@ export const Container = styled.div`
     z-index: 0;
     overflow: hidden;
     background: radial-gradient(circle, rgba(243,149,64,1) 20%, rgba(61,13,1,1) 90%, rgba(0,0,0,1) 100%);
-`
-
+`;
 
 export const FormWrap = styled.div`
     height: 100%;
@@ -23,8 +22,7 @@ export const FormWrap = styled.div`
     @media screen and (max-width: 400px) {
         height: 80%;
     }
-`
-
+`;
 
 export const Icon = styled(Link)`
     margin-left: 32px;
@@ -38,8 +36,7 @@ export const Icon = styled(Link)`
         margin-left: 16px;
         margin-top: 8px;
     }
-`
-
+`;
 
 export const FormContent = styled.div`
     height: 100%;
@@ -50,7 +47,7 @@ export const FormContent = styled.div`
     @media screen and (max-width: 480px) {
         padding: 10px;
     }
-`
+`;
 
 export const Form = styled.form`
     background: #010101;
@@ -67,7 +64,7 @@ export const Form = styled.form`
     @media screen and (max-width: 400px) {
         padding: 32px 32px;
     }
-`
+`;
 
 export const FormH1 = styled.h1`
     margin-bottom: 40px;
@@ -75,20 +72,20 @@ export const FormH1 = styled.h1`
     font-size: 20px;
     font-weight: 400;
     text-align: center;
-`
+`;
 
 export const FormLabel = styled.label`
     margin-bottom: 8px;
     font-size: 14px;
     color: #fff;
-`
+`;
 
 export const FormInput = styled.input`
     padding: 16px 16px;
     margin-bottom: 32px;
     border: none;
     border-radius: 4px;
-`
+`;
 
 export const FormButton = styled.button`
     background: #F39540;
@@ -98,10 +95,19 @@ export const FormButton = styled.button`
     color: #fff;
     font-size: 20px;
     cursor: pointer;
-`
+`;
 
 export const Text = styled.span`
     text-align: center;
     margin-top: 24px;
     color: #fff;
     font-size: 14px;
+`;    
+
+
+export const Text2 = styled.span`
+    text-align: center;
+    margin-top: 30px;
+    color: #fff;
+    font-size: 10px;
+`;    
